8b8ef7d3d1de3a529ab62ec2270e2e08c90b90c1
ravindra rawat <ravinrawat.wd@gmail.com>
2018-11-28 22:23:20 -0700

    added book rootquery

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -5,17 +5,21 @@ const { GraphQLObjectType, GraphQLString } = graphql;
 const BooksType = new GraphQLObjectType({
   name: 'Book',
   fields: () => ({
-    id: { type: GraphQLObjectType },
+    id: { type: GraphQLString },
     name: { type: GraphQLString },
     genre: { type: GraphQLString }
   })
 });
 
-const AuthorType = new GraphQLObjectType({
-  name: 'Author',
-  fields: () => ({
-    id: { type: GraphQLObjectType },
-    name: { type: GraphQLString },
-    age: { type: GraphQLString }
-  })
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    book: {
+      type: BookType,
+      args: { id: { type: GraphQLString } },
+      resolve(parent, args) {
+        // code to get data from db / other source
+      }
+    }
+  }
 });
