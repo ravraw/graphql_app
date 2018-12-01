c742409401a2f54fe0920aa282b21750ad0a50df
ravindra rawat <ravinrawat.wd@gmail.com>
2018-11-30 21:15:40 -0700

    added mutation for adAuthor -- database getting updated

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -11,8 +11,6 @@ const {
   GraphQLList
 } = graphql;
 
-// dummy data
-
 const BookType = new GraphQLObjectType({
   name: 'Book',
   fields: () => ({
@@ -77,6 +75,27 @@ const RootQuery = new GraphQLObjectType({
   }
 });
 
+const Mutation = new GraphQLObjectType({
+  name: 'Mutaion',
+  fields: {
+    addAuthor: {
+      type: AuthorType,
+      args: {
+        name: { type: GraphQLString },
+        age: { type: GraphQLInt }
+      },
+      resolve(parent, args) {
+        let author = new Author({
+          name: args.name,
+          age: args.age
+        });
+        return author.save();
+      }
+    }
+  }
+});
+
 module.exports = new GraphQLSchema({
-  query: RootQuery
+  query: RootQuery,
+  mutation: Mutation
 });
