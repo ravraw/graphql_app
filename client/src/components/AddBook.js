9df2d3cc52777903e31d54e385d19676131d503c
ravindra rawat <ravinrawat.wd@gmail.com>
2018-12-01 14:50:53 -0700

    state and onChange handlers to AddBook component

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -4,6 +4,20 @@ import { graphql } from 'react-apollo';
 import { getAuthorsQuery } from '../queries/queries';
 
 class AddBook extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      name: '',
+      genre: '',
+      authorId: ''
+    };
+  }
+
+  onSubmitHandler(e) {
+    e.preventDefault();
+    console.log(this.state);
+  }
+
   displayAuthors() {
     var data = this.props.data;
     if (data.loading) {
@@ -20,18 +34,32 @@ class AddBook extends Component {
   }
   render() {
     return (
-      <form id="add-book">
+      <form id="add-book" onSubmit={this.onSubmitHandler.bind(this)}>
         <div className="field">
           <label>Book name:</label>
-          <input type="text" />
+          <input
+            type="text"
+            onChange={e =>
+              this.setState({
+                name: e.target.value
+              })
+            }
+          />
         </div>
         <div className="field">
           <label>Genre:</label>
-          <input type="text" />
+          <input
+            type="text"
+            onChange={e =>
+              this.setState({
+                genre: e.target.value
+              })
+            }
+          />
         </div>
         <div className="field">
           <label>Author:</label>
-          <select>
+          <select onChange={e => this.setState({ authorId: e.target.value })}>
             <option>Select author</option>
             {this.displayAuthors()}
           </select>
