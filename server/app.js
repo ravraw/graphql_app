814a4cda6d186259f80685d0c1c18efc388af9ec
ravindra rawat <ravinrawat.wd@gmail.com>
2018-11-30 20:15:00 -0700

    connected to mlab database

--- server/app.js
+++ server/app.js
@@ -1,6 +1,18 @@
+require('dotenv').config();
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+
+const mongoose = require('mongoose');
+
+mongoose.connect(
+  `mongodb://${process.env.USERNAME}:${
+    process.env.PASSWORD
+  }@ds123454.mlab.com:23454/graphql`
+);
+mongoose.connection.once('open', () => {
+  console.log('connected to database....');
+});
 
 const app = express();
 
