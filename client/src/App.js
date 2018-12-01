e41dee48a83861db27c056badfc3e31b6fd3a057
ravindra rawat <ravinrawat.wd@gmail.com>
2018-12-01 12:37:22 -0700

    added packages for apollo

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 
+// componets
+
+import BookList from './components/BookList';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello</h1>
+        <h1>Book Store</h1>
+        <BookList />
       </div>
     );
   }
---
+++ client/src/components/BookList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class BookList extends Component {
+  render() {
+    return (
+      <div className="book__list">
+        <h1>Book List</h1>
+        <ul>
+          <li>Book 1</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default BookList;
